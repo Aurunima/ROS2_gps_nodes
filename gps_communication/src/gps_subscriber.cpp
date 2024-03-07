8253bd0afaa517b3629d1c4d8eac6f5b610e00d4
Aurunima <112366204+Aurunima@users.noreply.github.com>
2024-03-06 22:44:31 -0500

    Create gps_subscriber.cpp
    subscriber node of GPS communication

---
+++ gps_communication/src/gps_subscriber.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "gps_communication/msg/gps_data.hpp" // Include custom message type
+
+class GPSSubscriber : public rclcpp::Node {
+public:
+    GPSSubscriber() : Node("gps_subscriber") {
+        subscription_ = this->create_subscription<gps_communication::msg::GPSData>(
+            "gps_topic", 10, std::bind(&GPSSubscriber::gps_callback, this, std::placeholders::_1));
+    }
+
+private:
+    void gps_callback(const gps_communication::msg::GPSData::SharedPtr msg) {
+        RCLCPP_INFO(this->get_logger(), "Received GPS data: Latitude %.6f, Longitude %.6f, Altitude %.2f",
+                    msg->latitude, msg->longitude, msg->altitude);
+    }
+
+    rclcpp::Subscription<gps_communication::msg::GPSData>::SharedPtr subscription_;
+};
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<GPSSubscriber>());
+    rclcpp::shutdown();
+    return 0;
+}
